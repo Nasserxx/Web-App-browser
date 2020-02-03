938978b909d65178eb8c633239591837009a7bd8
Nasser Awad <nassern502@yahoo.com>
2020-02-03 09:08:04 +0100

    Add backend JavaScript file

--- src/backend.js
+++ src/backend.js
@@ -133,3 +133,13 @@
  
 // Implementation goes here
 
+/**
+ * backend JavaScript file
+ * Created on: 2020-02-03
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of backend functionality
+ */
+ 
+// Implementation goes here
+
