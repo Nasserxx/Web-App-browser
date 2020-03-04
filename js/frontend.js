abffcbb70f475da6c7e39ca3cade15221ed0be1d
Nasser Awad <nasser@nasser.com>
2020-03-04 14:31:15 +0100

    Add frontend JavaScript file

---
+++ js/frontend.js
@@ -0,0 +1,10 @@
+/**
+ * frontend JavaScript file
+ * Created on: 2020-03-04
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of frontend functionality
+ */
+ 
+// Implementation goes here
+
