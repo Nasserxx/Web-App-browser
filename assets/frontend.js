a7f3c224d45a387c6359b0127ae4d5bcef7b2d79
NasserXX <nasser@nasser.com>
2020-03-26 12:25:23 +0100

    Add frontend JavaScript file

--- assets/frontend.js
+++ assets/frontend.js
@@ -38,3 +38,13 @@
  
 // Implementation goes here
 
+/**
+ * frontend JavaScript file
+ * Created on: 2020-03-26
+ * Author: NasserXX
+ *
+ * Description: Implementation of frontend functionality
+ */
+ 
+// Implementation goes here
+
