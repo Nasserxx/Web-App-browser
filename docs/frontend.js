b7d39e8ddb4e93ee44b6fd1cfc295cdfc0e4ca13
Nasser Awad <nasser@nasser.com>
2020-03-30 13:30:55 +0200

    Add frontend JavaScript file

--- docs/frontend.js
+++ docs/frontend.js
@@ -64,3 +64,13 @@
  
 // Implementation goes here
 
+/**
+ * frontend JavaScript file
+ * Created on: 2020-03-30
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of frontend functionality
+ */
+ 
+// Implementation goes here
+
