9f3cebd51e56889d61e673e8836bf533f9c8fdf6
NasserXX <nasser@nasser.com>
2020-04-08 18:14:17 +0200

    Add frontend JavaScript file

--- css/frontend.js
+++ css/frontend.js
@@ -85,3 +85,13 @@
  
 // Implementation goes here
 
+/**
+ * frontend JavaScript file
+ * Created on: 2020-04-08
+ * Author: NasserXX
+ *
+ * Description: Implementation of frontend functionality
+ */
+ 
+// Implementation goes here
+
