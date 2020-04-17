61314e381b820e1efe524c3744a56366c4d3a859
Nasser Awad <nassern502@yahoo.com>
2020-04-17 05:18:50 +0200

    Add navigation JavaScript file

--- src/navigation.js
+++ src/navigation.js
@@ -88,3 +88,13 @@
 
 // Added pagination feature on 2022-08-28
 // Enhancing user experience
+/**
+ * navigation JavaScript file
+ * Created on: 2020-04-17
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of navigation functionality
+ */
+ 
+// Implementation goes here
+
