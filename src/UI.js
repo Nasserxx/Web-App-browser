9b9d236b24f66fd3f1a21bb5004f1525c2283e86
Nasser Awad <nasser@nasser.com>
2020-05-07 12:32:36 +0200

    Add UI JavaScript file

--- src/UI.js
+++ src/UI.js
@@ -8,3 +8,13 @@
  
 // Implementation goes here
 
+/**
+ * UI JavaScript file
+ * Created on: 2020-05-07
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of UI functionality
+ */
+ 
+// Implementation goes here
+
