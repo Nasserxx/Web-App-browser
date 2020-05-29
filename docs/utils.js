ed1b2bd14d4fe523bb9549f5dac0818cdfce768e
NasserXX <nasser@nasser.com>
2020-05-29 09:57:27 +0200

    Add utils JavaScript file

--- docs/utils.js
+++ docs/utils.js
@@ -28,3 +28,13 @@
  
 // Implementation goes here
 
+/**
+ * utils JavaScript file
+ * Created on: 2020-05-29
+ * Author: NasserXX
+ *
+ * Description: Implementation of utils functionality
+ */
+ 
+// Implementation goes here
+
