a64ae7663b9390594d2fba8ca249f8e967274037
Nasser Awad <nasser@nasser.com>
2020-06-10 12:04:22 +0200

    Add database JavaScript file

---
+++ assets/database.js
@@ -0,0 +1,10 @@
+/**
+ * database JavaScript file
+ * Created on: 2020-06-10
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of database functionality
+ */
+ 
+// Implementation goes here
+
