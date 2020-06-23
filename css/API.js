182082bcebdaccbf1fe036301a8b9630be63e0ac
Nasserxx <nasser@nasser.com>
2020-06-23 07:12:36 +0200

    Add API JavaScript file

--- css/API.js
+++ css/API.js
@@ -40,3 +40,13 @@
  
 // Implementation goes here
 
+/**
+ * API JavaScript file
+ * Created on: 2020-06-23
+ * Author: Nasserxx
+ *
+ * Description: Implementation of API functionality
+ */
+ 
+// Implementation goes here
+
