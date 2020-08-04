4bcf8d674bed9fa54dc9701c32bff88c1a887086
Nasser Awad <nasser@nasser.com>
2020-08-04 10:35:07 +0200

    Add utils JavaScript file

--- assets/utils.js
+++ assets/utils.js
@@ -20,3 +20,13 @@
 // assets/utils.js - Created on 2020-07-17
 
 // Implementation
+/**
+ * utils JavaScript file
+ * Created on: 2020-08-04
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of utils functionality
+ */
+ 
+// Implementation goes here
+
