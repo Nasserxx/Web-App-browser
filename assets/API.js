2cee0a73ffd2ff23066149886e0de1eb5dbb41bb
Nasser Awad <nasser@nasser.com>
2020-08-07 12:23:31 +0200

    Add API JavaScript file

--- assets/API.js
+++ assets/API.js
@@ -21,3 +21,13 @@
 // assets/API.js - Created on 2020-06-28
 
 // Implementation
+/**
+ * API JavaScript file
+ * Created on: 2020-08-07
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of API functionality
+ */
+ 
+// Implementation goes here
+
