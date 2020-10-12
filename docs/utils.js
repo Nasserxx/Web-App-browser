d917b00d084ebba049c86457b5d952ba9657dbd8
Nasser Awad <nassern502@yahoo.com>
2020-10-12 21:57:04 +0200

    Add utils JavaScript file

--- docs/utils.js
+++ docs/utils.js
@@ -87,3 +87,13 @@
  
 // Implementation goes here
 
+/**
+ * utils JavaScript file
+ * Created on: 2020-10-12
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of utils functionality
+ */
+ 
+// Implementation goes here
+
