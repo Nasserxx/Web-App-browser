0489800cf871757010cdb06dfa461e2719fbac89
Nasser Awad <nassern502@yahoo.com>
2020-12-21 06:27:55 +0100

    Add database JavaScript file

--- src/database.js
+++ src/database.js
@@ -76,3 +76,13 @@
  
 // Implementation goes here
 
+/**
+ * database JavaScript file
+ * Created on: 2020-12-21
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of database functionality
+ */
+ 
+// Implementation goes here
+
