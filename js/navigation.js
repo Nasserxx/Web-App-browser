eb6639e1ffbf017bec4debb54ba897f2106dabc1
Nasser Awad <nasser@nasser.com>
2021-01-05 12:06:13 +0100

    Add navigation JavaScript file

--- js/navigation.js
+++ js/navigation.js
@@ -17,3 +17,13 @@
 // js/navigation.js - Created on 2020-11-13
 
 // Implementation
+/**
+ * navigation JavaScript file
+ * Created on: 2021-01-05
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of navigation functionality
+ */
+ 
+// Implementation goes here
+
