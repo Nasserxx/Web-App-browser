0ffffb26c9e330b0eb35e21a254cb75daf25bc31
Nasser Awad <nasser@nasser.com>
2021-01-25 15:19:57 +0100

    Add layout JavaScript file

--- assets/layout.js
+++ assets/layout.js
@@ -1,3 +1,13 @@
 // assets/layout.js - Created on 2020-09-11
 
 // Implementation
+/**
+ * layout JavaScript file
+ * Created on: 2021-01-25
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of layout functionality
+ */
+ 
+// Implementation goes here
+
