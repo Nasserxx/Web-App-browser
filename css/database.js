fffee46d409ff0d2986b25237fbc64ef9fb0cb94
Nasser Awad <nassern502@yahoo.com>
2021-04-18 23:13:53 +0200

    Add database JavaScript file

--- css/database.js
+++ css/database.js
@@ -124,3 +124,13 @@
 
 // Added core functionality on 2021-04-12
 // Implementing essential features
+/**
+ * database JavaScript file
+ * Created on: 2021-04-18
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of database functionality
+ */
+ 
+// Implementation goes here
+
