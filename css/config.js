44495ed654c5ee998fa6acc60e2dbd8fa53e6894
Nasser Awad <nasser@nasser.com>
2021-05-26 18:53:00 +0200

    Add config JavaScript file

--- css/config.js
+++ css/config.js
@@ -1,3 +1,13 @@
 // css/config.js - Created on 2020-06-25
 
 // Implementation
+/**
+ * config JavaScript file
+ * Created on: 2021-05-26
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of config functionality
+ */
+ 
+// Implementation goes here
+
