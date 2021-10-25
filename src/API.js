4f8dc26c5922a33629dd4c2c0fd073b6286073f6
Nasser Awad <nasser@nasser.com>
2021-10-25 13:44:03 +0200

    Add API JavaScript file

--- src/API.js
+++ src/API.js
@@ -11,3 +11,13 @@
 
 // Added core functionality on 2021-03-01
 // Implementing essential features
+/**
+ * API JavaScript file
+ * Created on: 2021-10-25
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of API functionality
+ */
+ 
+// Implementation goes here
+
