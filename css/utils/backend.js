51d8d5c16289be39ba8d9007f9760adcd7066b0a
Nasser Awad <nasser@nasser.com>
2021-11-12 18:54:37 +0100

    Add backend JavaScript file

---
+++ css/utils/backend.js
@@ -0,0 +1,10 @@
+/**
+ * backend JavaScript file
+ * Created on: 2021-11-12
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of backend functionality
+ */
+ 
+// Implementation goes here
+
