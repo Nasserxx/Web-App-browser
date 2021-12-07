c88308a63d2d57c50d269ba875bc30702b31e2e8
Nasser Awad <nassern502@yahoo.com>
2021-12-07 16:10:04 +0100

    Add navigation JavaScript file

---
+++ src/helpers/navigation.js
@@ -0,0 +1,10 @@
+/**
+ * navigation JavaScript file
+ * Created on: 2021-12-07
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of navigation functionality
+ */
+ 
+// Implementation goes here
+
