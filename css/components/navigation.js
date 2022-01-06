03077c3838d9ad44e7695d1d42d6ba50097ab4d9
Nasserxx <nasser@nasser.com>
2022-01-06 10:38:46 +0100

    feat: Add navigation JavaScript with import support

---
+++ css/components/navigation.js
@@ -0,0 +1,10 @@
+/**
+ * navigation JavaScript file
+ * Created on: 2022-01-06
+ * Author: Nasserxx
+ *
+ * Description: Implementation of navigation functionality
+ */
+ 
+// Implementation goes here
+
