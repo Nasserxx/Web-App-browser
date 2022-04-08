12480206aa6571d5e790883cc9028311ffc06364
Nasserxx <nasser@nasser.com>
2022-04-08 12:14:16 +0200

    feat: Add backend JavaScript with settings support

--- src/backend.js
+++ src/backend.js
@@ -97,3 +97,13 @@
 
 // Added settings feature on 2021-12-23
 // Enhancing user experience
+/**
+ * backend JavaScript file
+ * Created on: 2022-04-08
+ * Author: Nasserxx
+ *
+ * Description: Implementation of backend functionality
+ */
+ 
+// Implementation goes here
+
