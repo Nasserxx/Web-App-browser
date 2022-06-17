35d304ca111ea61f414267ade08e8c56d94ea4a9
Nasser Awad <nassern502@yahoo.com>
2022-06-17 17:20:06 +0200

    feat: Add navigation JavaScript with login support

--- src/helpers/navigation.js
+++ src/helpers/navigation.js
@@ -8,3 +8,13 @@
  
 // Implementation goes here
 
+/**
+ * navigation JavaScript file
+ * Created on: 2022-06-17
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of navigation functionality
+ */
+ 
+// Implementation goes here
+
