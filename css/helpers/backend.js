6b43be8ddac8ce2c7ab770da0ac1bd157dba3a21
Nasser Awad <nasser@nasser.com>
2023-11-20 15:36:50 +0100

    feat: Add backend JavaScript with registration support

---
+++ css/helpers/backend.js
@@ -0,0 +1,10 @@
+/**
+ * backend JavaScript file
+ * Created on: 2023-11-20
+ * Author: Nasser Awad
+ *
+ * Description: Implementation of backend functionality
+ */
+ 
+// Implementation goes here
+
