1eada98ed72f2de7b89216ae9bac2e9e9c88da0e
Nasserxx <nasser@nasser.com>
2024-12-25 16:35:41 +0100

    feat: Add frontend JavaScript with import support

--- css/helpers/frontend.js
+++ css/helpers/frontend.js
@@ -8,3 +8,13 @@
  
 // Implementation goes here
 
+/**
+ * frontend JavaScript file
+ * Created on: 2024-12-25
+ * Author: Nasserxx
+ *
+ * Description: Implementation of frontend functionality
+ */
+ 
+// Implementation goes here
+
